0298bfcee5f09f1c6c66b6e8495b6eb4dcafa225
ogakento <ogasawara.kento.22@shizuoka.ac.jp>
2022-07-30 14:22:23 +0900

    public class に変更

--- 14/PrintDifImageHtml.java
+++ 14/PrintDifImageHtml.java
@@ -5,16 +5,23 @@ public class PrintDifImageHtml {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        System.out.print("<HTML>\n");
-        System.out.print("<HEAD><TITLE>Single Image Page </TITLE></HEAD>\n");
-        System.out.print("<BODY>\n");
+        if (n <= 10 && n > 0) {
 
-        for (int i = 0; i < n; i++) {
-        System.out.print("<IMG SRC=\"" + i + ".jpg\"><BR>\n");
+            System.out.print("<HTML>\n");
+            System.out.print("<HEAD><TITLE>Single Image Page </TITLE></HEAD>\n");
+            System.out.print("<BODY>\n");
+
+            for (int i = 0; i < n; i++) {
+                System.out.print("<IMG SRC=\"" + i + ".jpg\"><BR>\n");
+            }
+
+            System.out.print("</BODY>\n");
+            System.out.print("</HTML>\n");
+            System.err.print(n + "枚の画像を表示しました");
+
+        } else {
+            System.err.print("1以上10以下の値を入力してください");
         }
 
-        System.out.print("</BODY>\n");
-        System.out.print("</HTML>\n");
-        System.err.print(n + "枚の画像を表示しました");
-    } 
+    }
 }
